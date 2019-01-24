c4c8b1b1f941a698baaf6d826e24a357761a6165
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-01-24 20:18:48 +0530

    setup database

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ var express = require('express'),
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:3000/blog_app');
 
+var blogSchema={
+                   title : String,
+                   image: String,
+                   desc: String,
+                    date:Date.now();
+                }
+var blogdb=mongoose.model('blogdb',blogSchema);
 
 app.get('/',function (req,res) {
 res.send('Hello');
