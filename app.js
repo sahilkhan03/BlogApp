91e1ddcf1e5f85267c98d48176a6603c6ecc16de
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-01-24 21:45:17 +0530

    add route and page for home

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 var express = require('express'),
-    mongoose= require('mongoose'),
-    bodyParser=require('body-parser'),
-    app=express();
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    app = express();
 
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended : true}));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-mongoose.connect('mongodb://localhost:3000/blog_app');
+mongoose.connect('mongodb://localhost/blog_app',{useNewUrlParser: true});
 
-var blogSchema={
-                   title : String,
-                   image: String,
-                   desc: String,
-                    date:Date.now();
-                }
-var blogdb=mongoose.model('blogdb',blogSchema);
-
-app.get('/',function (req,res) {
-res.send('Hello');
+var blogSchema = {
+    title: String,
+    image: String,
+    desc: String,
+    date: {type: Date , default: Date.now}
+};
+var blogdb = mongoose.model('blogdb', blogSchema);
+blogdb.create({
+   title: 'New',
+   image: 'sadfnn',
+    desc:  'safnv'
 });
 
-app.listen(3000,function () {
+app.get('/', function (req, res) {
+     blogdb.find({},function (err,blogs) {
+       if(err)
+           console.log(err);
+       else
+       res.render('index', {blogs: blogs});
+   });
+
+});
+
+app.listen(3000, function () {
     console.log("Server started at port 3000");
 });
 
