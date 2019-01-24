a676fc0f6d74bb5723d0d704170c664c2e6cb958
Md. Sahil Khan <sahilkhan786154@gmail.com>
2019-01-24 21:59:26 +0530

    add partials

--- app.js
+++ app.js
@@ -6,28 +6,28 @@ var express = require('express'),
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
-mongoose.connect('mongodb://localhost/blog_app',{useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/blog_app', {useNewUrlParser: true});
 
 var blogSchema = {
     title: String,
     image: String,
     desc: String,
-    date: {type: Date , default: Date.now}
+    date: {type: Date, default: Date.now}
 };
 var blogdb = mongoose.model('blogdb', blogSchema);
 blogdb.create({
-   title: 'New',
-   image: 'sadfnn',
-    desc:  'safnv'
+    title: 'New',
+    image: 'sadfnn',
+    desc: 'safnv'
 });
 
 app.get('/', function (req, res) {
-     blogdb.find({},function (err,blogs) {
-       if(err)
-           console.log(err);
-       else
-       res.render('index', {blogs: blogs});
-   });
+    blogdb.find({}, function (err, blogs) {
+        if (err)
+            console.log(err);
+        else
+            res.render('index', {blogs: blogs});
+    });
 
 });
 
